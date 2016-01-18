e93b201d67f6f03a5397b88cf40f8c02d09ebfcf
xingx <1012315920@qq.com>
2016-01-18 11:06:24 +0800

    BarcodeView的格式更改

--- do_BarcodeView/source/do_BarcodeView/view/weixin/index.ui.js
+++ do_BarcodeView/source/do_BarcodeView/view/weixin/index.ui.js
@@ -1,5 +1,12 @@
 var app = sm("do_App");
 var page = sm("do_Page");
+var close = ui("close");
+close.on("touch", function() {
+	app.closePage();
+})
+page.on("back", function(data) {
+	app.closePage();
+})
 
 var iv0 = ui("iv0");
 var iv1 = ui("iv1");
